eb883a88efb7bba8b2a2dc9135749d937d4143d5
Ricardo Pablo De Leon <ricardopablo.deleon@gmail.com>
2018-10-06 20:06:13 +0800

    Add test parser code

--- routes/parse.js
+++ routes/parse.js
@@ -3,7 +3,17 @@ const router = express.Router();
 const htmlToJson = require("html-to-json");
 
 router.get("/:page", (req, res, next) => {
+  var promise = htmlToJson.request(
+    "https://sil-philippines-languages.org/online/msm/lexicon/01.htm",
+    {
+      function() {}
+    }
+  );
   res.send("Parser " + req.params.page);
+
+  promise.done(result => {
+    console.log(result);
+  });
 });
 
 module.exports = router;
