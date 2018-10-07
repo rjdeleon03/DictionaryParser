9f36a037ae4f525b8d487cb670b75bac008b032a
Ricardo Pablo De Leon <ricardopablo.deleon@gmail.com>
2018-10-07 13:33:30 +0800

    Correctly parse pages

--- routes/parse.js
+++ routes/parse.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const router = express.Router();
 const htmlToJson = require("html-to-json");
 
-const subEntryTag = "lp_LexSubEntryPara";
+const entryTag = "lp_LexEntryPara";
 const MAX_ENTRY_COUNT = 26;
 
 let currParentId = -1;
@@ -12,7 +12,12 @@ let dictEntries = [];
 router.get("/:page", async (req, res, next) => {
   res.send("Parser " + req.params.page);
 
-  performParseProcess(1, "https://sil-philippines-languages.org/online/msm/lexicon/" + getFormattedIdx(1) + ".htm");
+  performParseProcess(
+    1,
+    "https://sil-philippines-languages.org/online/msm/lexicon/" +
+      getFormattedIdx(1) +
+      ".htm"
+  );
 });
 
 module.exports = router;
@@ -23,27 +28,31 @@ function getFormattedIdx(number) {
 
 function performParseProcess(idx, url) {
   console.log(url);
-  var promise = htmlToJson.request(
-    url,
-    {
-      'entries': ['p', (block) => parseEntry(block)]
-    }
-  );
+  var promise = htmlToJson.request(url, {
+    entries: ["p", block => parseEntry(block)]
+  });
 
   promise.done(result => {
     // console.log(result.subEntries.length);
     dictEntries = dictEntries.concat(result.entries);
-    console.log(idx + " >> " + result.entries.length + " --- " + dictEntries.length);
+    console.log(
+      idx + " >> " + result.entries.length + " --- " + dictEntries.length
+    );
 
     if (idx < MAX_ENTRY_COUNT) {
       idx++;
-      performParseProcess(idx, "https://sil-philippines-languages.org/online/msm/lexicon/" + getFormattedIdx(idx) + ".htm");
+      performParseProcess(
+        idx,
+        "https://sil-philippines-languages.org/online/msm/lexicon/" +
+          getFormattedIdx(idx) +
+          ".htm"
+      );
     } else {
       cleanupList();
 
-      fs.writeFile("dict.json", JSON.stringify(dictEntries), function (err) {
+      fs.writeFile("dict.json", JSON.stringify(dictEntries), function(err) {
         if (err) throw err;
-        console.log('complete');
+        console.log("complete");
       });
     }
   });
@@ -51,37 +60,34 @@ function performParseProcess(idx, url) {
 
 /**
  * Parses the given html block
- * @param {*} block 
+ * @param {*} block
  */
 function parseEntry(block) {
-
-  let entryName = block.find('.lp_LexEntryName');
+  let entryWord = block.find(".lp_LexEntryName");
   let parentId = -1;
 
-  if (block[0].attribs.class !== subEntryTag) {
-    currParentId = entryName.attr('id');
+  if (block[0].attribs.class === entryTag) {
+    currParentId = entryWord.attr("id");
   } else {
     parentId = currParentId;
   }
 
-  var word = entryName.text();
-  var partOfSpeech = block.find('.lp_PartOfSpeech').text();
-  var meaning = block.find('.lp_Gloss_English').text();
-  var note = block.find('.lp_MiniHeading').text();
-  var relatedWord = block.find('.lp_MainCrossRef').text();
-
-  if (!word || word.trim().length === 0) return;
+  var word = entryWord.text();
+  var partOfSpeech = block.find(".lp_PartOfSpeech").text();
+  var meaning = block.find(".lp_Gloss_English").text();
+  var note = block.find(".lp_MiniHeading").text();
+  var relatedWord = block.find(".lp_MainCrossRef").text();
 
   return new Entry(parentId, word, partOfSpeech, meaning, note, relatedWord);
 }
 
 /**
  * Dictionary entry model
- * @param {*} word 
- * @param {*} partOfSpeech 
- * @param {*} meaning 
- * @param {*} note 
- * @param {*} relatedWord 
+ * @param {*} word
+ * @param {*} partOfSpeech
+ * @param {*} meaning
+ * @param {*} note
+ * @param {*} relatedWord
  */
 function Entry(parentId, word, partOfSpeech, meaning, note, relatedWord) {
   this.parentId = parentId;
@@ -94,8 +100,14 @@ function Entry(parentId, word, partOfSpeech, meaning, note, relatedWord) {
 
 function cleanupList() {
   if (dictEntries) {
-    dictEntries.forEach(entry => {
-      if (!entry) dictEntries.pop(entry);
+    console.log(dictEntries.length);
+    dictEntries.forEach((entry, idx) => {
+      if (
+        (!entry.word || entry.word.trim().length === 0) &&
+        (!entry.meaning || entry.meaning.trim().length === 0)
+      )
+        dictEntries.splice(idx, 1);
     });
+    console.log(dictEntries.length);
   }
 }
